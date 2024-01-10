405a7b557e8685324882dc48220a8a000b4b9f1d
neverbot <ivan@neverbot.com>
2024-01-10 19:04:22 +0100

    chore: add badge block tag

---
+++ scripts/badge.js
@@ -0,0 +1,9 @@
+hexo.extend.tag.register(
+  'badge',
+  function (args) {
+    const [color, content] = args;
+
+    return '<span class="badge ' + color + '">' + content + '</span>';
+  },
+  { ends: false, async: false },
+);
